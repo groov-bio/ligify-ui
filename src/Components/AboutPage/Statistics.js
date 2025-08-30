d55ab6b0bd4219f70dd2f76b7d72805aaaa61f31
Simon d'Oelsnitz <simonsnitz@gmail.com>
2025-08-30 10:21:49 -0400

    added some DB stats plots and description

--- src/Components/AboutPage/Statistics.js
+++ src/Components/AboutPage/Statistics.js
@@ -13,27 +13,48 @@ export default function Statistics() {
     >
       <Typography
         sx={{ fontSize: { xs: 24, sm: 28, md: 32 } }}
-        fontWeight="300"
+        fontWeight="500"
         gutterBottom
       >
         Statistics
       </Typography>
-      <Typography sx={{ fontSize: { xs: 14, sm: 16, md: 18 } }}>
-        Words
-      </Typography>
 
-
-      {/* <Typography
-        sx={{ fontSize: { xs: 24, sm: 28, md: 32 } }}
-        mt={5}
+      <Typography
+        sx={{ fontSize: { xs: 18, sm: 22, md: 24 } }}
+        mt={3}
         fontWeight="300"
         gutterBottom
       >
-        Creating Ligify<sup>DB</sup>
-      </Typography>
+        Regulators
+      </Typography >
+
       <Typography sx={{ fontSize: { xs: 14, sm: 16, md: 18 } }}>
-        Words
-      </Typography> */}
+        Regulators within Ligify<sup>DB</sup> belong to some of the most common structural families, including LysR and TetR.
+        Compared to the past analyses of structural family frequencies in all bacteria, the LacI family is more represented and 
+        the AraC family is less represented.
+
+        Most regulators are between 150-350 amino acids long, as expected. Peaks around lengths of 180, 230, and 320 likely correspond
+        to the TetR/MarR, LysR, and LacI families, respectively.
+      </Typography>
+
+      <Box
+          component="img"
+          mt={3}
+          sx={{ mr:{sm:0, md:"9%"}, width:{sm:"100%", md:'45%'} }}
+          // sx={{ ml: {xs:'10%', sm: "15%", md: "20%", lg:"25%"}, width:{xs:"80%", sm:'70%', md:"60%", lg:'50%'} }}
+          src={'/Content_Stats/Common_Families.png'}
+          alt="Common_Families"
+        />
+
+      <Box
+          component="img"
+          mt={3}
+          sx={{ width:{sm:"100%", md:'45%'} }}
+          // sx={{ ml: {xs:'10%', sm: "15%", md: "20%", lg:"25%"}, width:{xs:"80%", sm:'70%', md:"60%", lg:'50%'} }}
+          src={'/Content_Stats/Regulator_Lengths.png'}
+          alt="Regulator_Lengths"
+        />
+
 
     </Box>
   );
