cda6ae52051813150d90d17e8ad862745439c349
Simon d'Oelsnitz <simonsnitz@gmail.com>
2025-08-31 11:07:07 -0400

    added promoter and orientation stats

--- src/Components/AboutPage/Statistics.js
+++ src/Components/AboutPage/Statistics.js
@@ -56,6 +56,42 @@ export default function Statistics() {
         />
 
 
+
+<Typography
+        sx={{ fontSize: { xs: 18, sm: 22, md: 24 } }}
+        mt={3}
+        fontWeight="300"
+        gutterBottom
+      >
+        Promoters and Orientation
+      </Typography >
+
+      <Typography sx={{ fontSize: { xs: 14, sm: 16, md: 18 } }}>
+        Predicted promoters are about 150bp in length, on average, with a trailing tail up to ~1000bp. 
+
+        Regulators are most often expressed in the opposite direction of the associated enzyme (Divergent, 73.7%) compared to 
+        being expressed in the same direction (Convergent, 26.3%). These ratios are expected, based on what has been reported
+        in the literature for several transcription regulator families.
+      </Typography>
+
+      <Box
+          component="img"
+          mt={3}
+          sx={{ mr:{sm:0, md:"9%"}, width:{sm:"100%", md:'45%'} }}
+          // sx={{ ml: {xs:'10%', sm: "15%", md: "20%", lg:"25%"}, width:{xs:"80%", sm:'70%', md:"60%", lg:'50%'} }}
+          src={'/Content_Stats/Promoter_Lengths.png'}
+          alt="Promoter_Lengths"
+        />
+
+      <Box
+          component="img"
+          mt={3}
+          sx={{ width:{sm:"100%", md:'45%'} }}
+          // sx={{ ml: {xs:'10%', sm: "15%", md: "20%", lg:"25%"}, width:{xs:"80%", sm:'70%', md:"60%", lg:'50%'} }}
+          src={'/Content_Stats/Regulator_Orientation.png'}
+          alt="Regulator_Orientation"
+        />
+
     </Box>
   );
 }
